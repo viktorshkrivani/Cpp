700e67baf0b24c27961a1166d9d9522c8189f9f7
Viktor Shkrivani <122412837+viktorshkrivani@users.noreply.github.com>
2025-08-27 19:00:05 -0400

    Update FileName.cpp

--- Ass2/Project2/Project2/FileName.cpp
+++ Ass2/Project2/Project2/FileName.cpp
@@ -1,35 +1,64 @@
+/*==========================================================
+
+ Title:       Assignment 2 - Numerical Representation
+ Course:      CIS 2252
+ Author:      <Viktor Shkrivani>
+ Date:        <05/28/2023 >
+ Description: 
+
+ ==========================================================
+*/
 
 #include <iostream>
-#include <algorithm>
-using namespace std;
+#include <unordered_map>
 
-int main() {
-    int size;
-    cout << "Enter the number of elements: ";
-    cin >> size;
 
-    int arr[size];
-    cout << "Enter the elements:\n";
-    for (int i = 0; i < size; i++) {
-        cin >> arr[i];
+std::string numberToWords(int number) {
+
+    std::unordered_map<int, std::string> words = {
+    {1, "one"},
+    {2, "two"},
+    {3, "three"},
+    {4, "four"},
+    {5, "five"},
+    {6, "six"},
+    {7, "seven"},
+    {8, "eight"},
+    {9, "nine"}
+  };
+
+    if (words.count(number) > 0){
+    return words[number];
     }
 
-    cout << "Before sorting: ";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-
-    sort(arr, arr + size);
-
-    cout << "\nAfter sorting: ";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-
-    cout << "\nPrinting in reverse order: ";
-    for (int i = size - 1; i >= 0; i--) {
-        cout << arr[i] << " ";
-    }
-
-    return 0;
+  return "";
+ 
 }
+ 
+ 
+int main() {
+    
+    int num1;
+    std::cin >> num1;
+    
+    int num2;
+    std::cin >> num2;
+    
+    for (int i = num1; i <= num2; ++i) {
+        if (i >= 1 && i <=9) {
+            std::string word = numberToWords(i);
+            std::cout << word << std::endl;
+        }
+        else if (i % 2 == 0) {
+            std::cout << "even" << std::endl;
+        } else {
+            std::cout << "odd" << std::endl;
+        }
+    }
+
+  
+                   
+  return 0;
+}
+
+
