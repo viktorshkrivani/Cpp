bd31d21314f2b9e8437e52e146d81a4e57a94e12
Viktor Shkrivani <122412837+viktorshkrivani@users.noreply.github.com>
2025-08-27 17:48:20 -0400

    Update FileName.cpp

--- Ass1/Project1/Project1/FileName.cpp
+++ Ass1/Project1/Project1/FileName.cpp
@@ -1,49 +1,25 @@
 #include <iostream>
-#include <unordered_map>
-
-std::string numberToWord(int number) {
-    std::unordered_map<int, std::string> words = {
-        {1, "one"},
-        {2, "two"},
-        {3, "three"},
-        {4, "four"},
-        {5, "five"},
-        {6, "six"},
-        {7, "seven"},
-        {8, "eight"},
-        {9, "nine"}
-    };
-
-    if (words.count(number) > 0) {
-        return words[number];
-    }
-
-    return "";  // Return empty string if number is not in range
-}
+#include <string>
+using namespace std;
 
 int main() {
-    int num1, num2;
+    string name;
+    double age;
+    cin >> name >> age;
 
-    std::cout << "Enter the first number: ";
-    std::cin >> num1;
+    double wizardAge;
 
-    std::cout << "Enter the second number: ";
-    std::cin >> num2;
-
-    std::cout << "Output: " << std::endl;
-
-    for (int i = num1; i <= num2; ++i) {
-        if (i >= 1 && i <= 9) {
-            std::string word = numberToWord(i);
-            std::cout << word << std::endl;
-        }
-        else if (i % 2 == 0) {
-            std::cout << "even" << std::endl;
-        }
-        else {
-            std::cout << "odd" << std::endl;
-        }
+    if (age <= 1.0) {
+        wizardAge = age * 10.5;
+    } else if (age <= 2.0) {
+        wizardAge = 21.0;
+    } else {
+        wizardAge = 21.0 + (age - 2.0) * 4.0;
     }
+
+    cout << "The dragon named " << name 
+         << " is " << wizardAge 
+         << " years old in wizard years." << endl;
 
     return 0;
 }
